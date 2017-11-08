a95266c8bde0ef04514c25b7a90a298775d062f9
Tim Ermilov <yamalight@gmail.com>
2017-11-08 19:18:43 +0100

    Dockerize keywords service and allow configuring it using env vars

--- keywords-processing-service/config.js
+++ keywords-processing-service/config.js
@@ -1,5 +1,5 @@
 // service config
 exports.ID = 'keywords';
 exports.type = 'processor';
-exports.rabbit = {host: 'localhost', exchange: 'datascience'};
+exports.rabbit = {host: process.env.RABBIT_HOST || 'localhost', exchange: 'datascience'};
 exports.resultKey = 'update';
