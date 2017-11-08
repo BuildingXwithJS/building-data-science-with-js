ec0287212c0da5a430ae483ba5c0afb3b73da7ce
Tim Ermilov <yamalight@gmail.com>
2017-11-08 19:18:08 +0100

    Dockerize fox service and allow configuring it using env vars

--- fox-processing-service/config.js
+++ fox-processing-service/config.js
@@ -1,6 +1,9 @@
 // service config
 exports.ID = 'fox';
 exports.type = 'processor';
-exports.rabbit = {host: 'localhost', exchange: 'datascience'};
+exports.rabbit = {
+  host: process.env.RABBIT_HOST || 'localhost',
+  exchange: 'datascience',
+};
 exports.resultKey = 'update';
-exports.fox = {url: 'http://localhost:4444/fox'};
+exports.fox = {url: process.env.FOX_URL || 'http://localhost:4444/fox'};
